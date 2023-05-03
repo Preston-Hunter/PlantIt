a2191ea92fb0c48ef84035953cf54b2f9a7403d5
PrestonHunter <prestonhunter2018@gmail.com>
2023-05-03 04:00:21 -0600

    basic front end functionality. Still need to do comments and the suggestor/helper

---
+++ client/plantit/components/PlantCard.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import {Platform } from "react-native"
+import {useNavigate} from "react-router-dom"
+
+export default function PlantCard({image, plantId, scientific_name, name, navigation}){
+    const [loaded, setLoaded] = useState(false)
+    const [plant, setPlant] = useState(null)
+    const navigate = useNavigate()
+
+
+    function handleClick(){
+        if (Platform.OS != "web"){
+            // todo: this
+            navigation.navigate("")
+            return "not web"
+        }
+        // import {Router}
+        navigate(`/plants/${plantId}`)  
+        return "web"
+    }
+
+    return (
+        <div style={{"flexDirection":"row","width":"20%", "height":"auto"}}>
+            <img src = {image} onClick={handleClick} style={{"width":"80%"}}/>
+            <br></br>
+            <Text>{scientific_name}{name!=null ? `(${name})`:""}</Text>
+        </div>
+    )
+
+}
